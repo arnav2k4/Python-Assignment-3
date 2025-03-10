f1345bf3f126ce03cf26b0c2cd04a33351241773
arnav2k4 <161723756+arnav2k4@users.noreply.github.com>
2025-03-11 02:55:13 +0530

    Create Task 2.py

---
+++ Task 2.py
@@ -0,0 +1,6 @@
+import math as mt
+
+num = int(input("Enter a number: "))
+print("Square root: {}".format(mt.sqrt(num)))
+print("Logarithm: {}".format(mt.log(num)))
+print("Sine: {}".format(mt.sin(num)))
